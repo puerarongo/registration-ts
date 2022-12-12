d624b7ff2b6c3e29e39b941e3a489b2881b88f76
puerarongo <akhardin96@gmail.com>
2022-12-12 21:57:28 +0200

    fix5

--- src/services/yupValidation/loginValidationSchema.ts
+++ src/services/yupValidation/loginValidationSchema.ts
@@ -4,12 +4,12 @@ export const loginValidationSchema = yup.object().shape({
     email: yup.string()
         .typeError("Will be a string")
         .email("The field contains an error")
-        .min(10, "The field contains an error")
-        .max(63, "The field contains an error")
+        .min(10, "The field must contain more than 10 characters")
+        .max(63, "The field must contain less than 63 characters")
         .required("This field cannot be empty"),
     password: yup.string()
         .typeError("Will be a string")
-        .min(5, "The field contains an error")
-        .max(30, "The field contains an error")
+        .min(5, "The field must contain more than 5 characters")
+        .max(30, "The field must contain less than 30 characters")
         .required("This field cannot be empty"),
 });
--- src/services/yupValidation/registrationValidationSchema.ts
+++ src/services/yupValidation/registrationValidationSchema.ts
@@ -4,17 +4,17 @@ export const registrationValidationSchema = yup.object().shape({
     email: yup.string()
         .typeError("Will be a string")
         .email("The field contains an error")
-        .min(10, "The field contains an error")
-        .max(63, "The field contains an error")
+        .min(10, "The field must contain more than 10 characters")
+        .max(63, "The field must contain less than 63 characters")
         .required("This field cannot be empty"),
     name: yup.string()
         .typeError("Will be a string")
-        .min(3, "The field contains an error")
-        .max(100, "The field contains an error")
+        .min(3, "The field must contain more than 3 characters")
+        .max(100, "The field must contain less than 100 characters")
         .required("This field cannot be empty"),
     password: yup.string()
         .typeError("Will be a string")
-        .min(5, "The field contains an error")
-        .max(30, "The field contains an error")
+        .min(5, "The field must contain more than 5 characters")
+        .max(30, "The field must contain less than 30 characters")
         .required("This field cannot be empty"),
 });
